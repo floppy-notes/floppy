9a8942d15a247c44b9887ece8784dee4f14ce6fe
GuilhermeCaruso <gui.caruso@hotmail.com>
2026-08-15 21:58:01 -0300

    feat(internal/item): add item structure and parser Close#9

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/floppy-notes/floppy/internal/item"
+)
 
 func main() {
-	fmt.Println("Hello Floppy")
+	rawData, err := os.ReadFile("testdata/floppy_vault/notes/2026/08/2026-08-12-0001-example.md")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(item.Parse(rawData))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/floppy-notes/floppy
 
 go 1.26.6
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
