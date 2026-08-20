589b671d97bf7d752b16c62acddd6328e952e675
GuilhermeCaruso <gui.caruso@hotmail.com>
2026-08-20 11:37:01 -0300

    feat(vault): add walk method to read files

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,38 +2,37 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/floppy-notes/floppy/internal/database"
-	"github.com/floppy-notes/floppy/internal/item"
+	"github.com/floppy-notes/floppy/internal/vault"
 )
 
 func main() {
+	fmt.Println(vault.Walk("testdata"))
 
-	db, err := database.Open(
-		database.WithPath("testdata"),
-	)
+	// db, err := database.Open(
+	// 	database.WithPath("testdata"),
+	// )
 
-	db.Conn.Ping()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	// db.Conn.Ping()
 
-	rawData, err := os.ReadFile("testdata/floppy_vault/notes/2026/08/2026-08-12-0001.example.md")
+	// rawData, err := os.ReadFile("testdata/floppy_vault/notes/2026/08/2026-08-12-0001.example.md")
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	parsedItem, err := item.Parse(rawData)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// parsedItem, err := item.Parse(rawData)
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = parsedItem.Front.Validate()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// err = parsedItem.Front.Validate()
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
 }
---
+++ internal/vault/vault.go
@@ -0,0 +1,46 @@
+package vault
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/floppy-notes/floppy/internal/item"
+)
+
+func Walk(vaultRootDir string) ([]item.Item, []error, error) {
+	items := []item.Item{}
+	var skipped []error
+
+	err := filepath.WalkDir(vaultRootDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() || !strings.HasSuffix(d.Name(), ".md") {
+			return nil
+		}
+
+		rawData, err := os.ReadFile(path)
+		if err != nil {
+			skipped = append(skipped, fmt.Errorf("skipping %s: %w", path, err))
+			return nil
+		}
+
+		parsedItem, err := item.Parse(rawData)
+		if err != nil {
+			skipped = append(skipped, fmt.Errorf("skipping %s: %w", path, err))
+			return nil
+		}
+		items = append(items, parsedItem)
+		return nil
+
+	})
+	if err != nil {
+		return nil, skipped, fmt.Errorf("walking vault: %w", err)
+	}
+	return items, skipped, nil
+
+}
